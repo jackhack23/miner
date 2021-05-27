2767944c86482a0cb64d5ada33f7cd1c5e0cf6c0
jackhack23 <84907587+jackhack23@users.noreply.github.com>
2021-05-27 16:36:02 +0530

    Update trx.sh

--- trx.sh
+++ trx.sh
@@ -2,5 +2,5 @@
 sudo apt update
 ! wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
 ! tar -xf lolMiner_v1.28a_Lin64.tar.gz
-%cd 1.28a/
+cd 1.28a/
 !./lolMiner --algo ETHASH --pool ethash.unmineable.com:5000 --user ETH:DQcUmP87e1Y2yTkjsG1dfaQ3VeeR2xEcRB.colab --ethstratum ETHPROXY
